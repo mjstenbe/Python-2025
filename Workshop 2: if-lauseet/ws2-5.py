b17a7052c87569e5ce241ae557b5b0e3fe40ec8a
Mika Stenberg <mika.stenberg@laurea.fi>
2025-03-27 09:58:35 +0000

    add

--- Workshop 2: if-lauseet/ws2-5.py
+++ Workshop 2: if-lauseet/ws2-5.py
@@ -1,28 +1,25 @@
-"""
-Kirjoita ohjelma, joka pyytää käyttäjältä päivän (1–31), kuukauden (1–12) ja vuoden. 
-Tarkista, onko annettu päivämäärä kelvollinen. Huomioi seuraavat asiat:
-•	Kuukaudet, joissa on 30 päivää: huhtikuu (4), kesäkuu (6), syyskuu (9), marraskuu (11).
-•	Helmikuussa (2) on normaalisti 28 päivää, mutta karkausvuonna 29 päivää.
-
-"""
-# Kirjoita ohjelma, joka pyytää käyttäjältä päivän (1–31), kuukauden (1–12) ja vuoden. 
+# Pyydetään käyttäjältä päivä, kuukausi ja vuosi
 paiva = int(input("Anna päivä: "))
 kk = int(input("Anna kuukausi: "))
 vuosi = int(input("Anna vuosi: "))
 
-# Suljetaan pois virheelliset kuukaudet
+# Tarkistetaan, onko kuukausi validi
 if kk < 1 or kk > 12:
     print("Virheellinen kuukausi!")
 
-# Kuukaudet, joissa on 30 päivää: huhtikuu (4), kesäkuu (6), syyskuu (9), marraskuu (11).
-elif kk == 4 or kk == 6 or kk == 9 or kk == 11:
+elif kk == 4 or kk == 6 or kk == 9 or kk == 11:  # Kuukaudet, joissa on 30 päivää
     if paiva < 1 or paiva > 30:
         print("Virheellinen määrä päiviä!")
-# Helmikuu
-elif kk == 2:
-     if paiva < 1 or paiva > 28:
-        print("Virheellinen määrä päiviä!")
+    else:
+        print("Kelvollinen päivämäärä.")
 
-# Oletetaan että muissa on 31 päivää
-elif paiva != 31:
-     print("Virheellinen määrä päiviä!")
+elif kk == 2:  # Helmikuu (28 päivää, ei tarkisteta karkausvuotta)
+    if paiva < 1 or paiva > 28:
+        print("Virheellinen määrä päiviä!")
+    else:
+        print("Kelvollinen päivämäärä.")
+        
+elif paiva < 1 or paiva > 31:  # Kuukaudet, joissa on 31 päivää
+    print("Virheellinen määrä päiviä!")
+else:
+    print("Kelvollinen päivämäärä.")
