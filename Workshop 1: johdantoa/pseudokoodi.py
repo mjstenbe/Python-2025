71f9cfc1f0852ef3d7bfd5d46e4c6b8d8b480edc
Mika Stenberg <mika.stenberg@laurea.fi>
2025-03-27 09:57:45 +0000

    add

--- Workshop 1: johdantoa/pseudokoodi.py
+++ Workshop 1: johdantoa/pseudokoodi.py
@@ -1,14 +1,3 @@
-"""
 Tee algoritmi, joka osaa laskea
 kaksi käyttäjän antamaa lukua yhteen ja
 tulostaa summan näytölle
-"""
-
-# Kysy luku1
-luku1 = input("Anna eka luku: ")
-
-# Kysy luku 2
-luku2 = input("Anna toka luku: ")
-
-# laskea kaksi käyttäjän antamaa lukua yhteen
-print(f"Tulos: {luku1+luku2}")
--- Workshop 2: if-lauseet/pseudokoodi.py
+++
@@ -1,3 +0,0 @@
-Tee algoritmi, joka osaa laskea
-kaksi käyttäjän antamaa lukua yhteen ja
-tulostaa summan näytölle
