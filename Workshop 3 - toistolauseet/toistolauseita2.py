f44263ba13abf28bc7d6d57f3eb0161cc5a30b51
Mika Stenberg <mika.stenberg@laurea.fi>
2025-04-10 09:52:33 +0000

    add

--- Workshop 3 - toistolauseet/toistolauseita2.py
+++ Workshop 3 - toistolauseet/toistolauseita2.py
@@ -1,6 +1,6 @@
 luku = 0
 
-while (luku < 21):
+while (luku < 6):
     print(f"Laskuri: {luku} ")
     # Kasvatuslause: lisätään luku-muuttujaan 1 
     luku=luku+1
---
+++ Workshop 3 - toistolauseet/tosistolauseita4.py
@@ -0,0 +1,4 @@
+i = 0
+while (i < 10):
+    print(f"Luku: {i}")
+    i = i + 3
