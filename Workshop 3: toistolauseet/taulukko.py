52c9de725fd6c56f5832572d15eed65213edf2cc
Mika Stenberg <mika.stenberg@laurea.fi>
2025-04-10 07:48:45 +0000

    add

---
+++ Workshop 3: toistolauseet/taulukko.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry", "grape", "lemon"]
+print("Taulukon pituus on: ", len(fruits))
+i = 0
+while ( i < len(fruits) ):
+    print( fruits[i] )
+    i = i+1
+
+for x in fruits:
+    print(x)
+ 
+ 
---
+++ Workshop 3: toistolauseet/toistoja.py
@@ -0,0 +1,15 @@
+rahaa = 20
+ulkonaSataa = True
+
+for i in range(10,25):
+    print(i)
+
+if (rahaa > 10 and ulkonaSataa == False ):
+    print("Käydään terassilla istahtamassa.")
+
+
+while(rahaa > 10 and ulkonaSataa):
+    print("Rahaa on vielä paljon!")
+    rahaa=rahaa-1
+
+
